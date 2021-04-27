e398690220f9d57fbc88fee2af6b564b328aa79b
Sampaio Leal <samp14hd@gmail.com>
2021-04-27 00:14:31 -0300

    Global styles and Home page added

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,32 @@ import {
 import NavBar from "./components/NavBar";
 import pokeTheme from "./styles/pokeTheme";
 import { ThemeProvider } from "styled-components";
+import Footer from "./components/Footer";
+import { CssBaseline } from "@material-ui/core";
+import GlobalStyles from "./styles/globals";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <StylesProvider injectFirst>
+      <CssBaseline />
+
       <MuiThemeProvider theme={pokeTheme}>
         <ThemeProvider theme={pokeTheme}>
           <Router>
             <NavBar />
 
             <Switch>
-              <Route exact path="/" />
+              <Route exact path="/" component={Home} />
               <Route exact path="/pokedex" />
             </Switch>
           </Router>
+
+          <Footer />
         </ThemeProvider>
       </MuiThemeProvider>
+
+      <GlobalStyles />
     </StylesProvider>
   );
 }
