27bdfcd264faba8c8b69819d9ca98544d16ebfc4
Sampaio Leal <samp14hd@gmail.com>
2021-04-27 00:13:29 -0300

    Container para lidar com a barra de navegação nas páginas

---
+++ src/components/Container/index.ts
@@ -0,0 +1,8 @@
+import { Container as MuiContainer } from "@material-ui/core";
+import styled from "styled-components";
+
+const Container = styled(MuiContainer)`
+  padding-top: 86px;
+`;
+
+export default Container;
